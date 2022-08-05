7b0a7e1dd91b69912d7b90a3a2b962ad8144a0f1
Conrad Russo <conradr@gmail.com>
2022-08-05 13:54:39 +0100

    initial file set up

---
+++ tests/room_test.py
@@ -0,0 +1,8 @@
+import unittest
+from classes.room import Room
+
+
+class TestRoom(unittest.TestCase):
+    
+    def setUp(self):
+        pass
---
+++ tests/song_test.py
@@ -0,0 +1,8 @@
+import unittest
+from classes.song import Song
+
+
+class TestSong(unittest.TestCase):
+
+    def setUp(self):
+        pass
